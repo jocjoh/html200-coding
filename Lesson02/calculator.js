8eb2762a1aac39cc84331d403e481e1d767657aa
jocjoh <jocjoh@uw.edu>
2019-09-17 16:09:17 -0700

    add code to java script doc

---
+++ Lesson02/calculator.js
@@ -0,0 +1,18 @@
+let firstName = 'Joce';
+let lastName = 'Johnson';
+const a = 2;
+const b = 10;
+const c = 20;
+const d = 100;
+
+console.log(firstName + ' ' + lastName)
+console.log(a * (b + c));
+console.log(c / a);
+console.log(-a);
+console.log((d / b) + (a * c) - b);
+console.log(d % a);
+console.log(firstName + b);
+console.log(lastName + firstName + d);
+console.log(a * b * c * d);
+console.log(d - (c + b) / a);
+console.log(c / b);
