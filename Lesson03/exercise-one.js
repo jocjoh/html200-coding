ec9662135ed6a5a90cf5792c5dbe2206cd977e83
jocjoh <jocjoh@uw.edu>
2019-09-26 16:48:37 -0700

    edit names of folders and add java to file

---
+++ Lesson03/exercise-one.js
@@ -0,0 +1,15 @@
+let summerFlowers = ['zinnia', 'dahlia', 'daisy', 'peony', 'marigold']
+
+console.log(summerFlowers);
+
+summerFlowers.push('sunflower');
+
+console.log(summerFlowers);
+
+summerFlowers.splice(2, 1);
+
+console.log(summerFlowers);
+
+const summerFlowersNote = summerFlowers.join(',');
+
+console.log(summerFlowersNote);
