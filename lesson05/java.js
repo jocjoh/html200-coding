2664aaed38629362e91ec5969c8bf449bc19b35d
jocjoh <jocjoh@uw.edu>
2019-10-11 16:30:51 -0700

    Add more javascript

--- lesson05/java.js
+++ lesson05/java.js
@@ -1,14 +1,29 @@
+let total = 0;
+
 function atm() {
-  let total = 0;
-  let money = prompt('Please enter a command.', 'D, W, B, or Q');
-  switch(money) {
-    case 'Q':
-      break;
-    case 'D':
-      prompt('Please enter the amount you want to deposit.');
-    case 'W':
-      prompt('Please enter the amount you want to withdraw.');
-    case 'B':
-      alert('Your balance is ' + total);
+  let i = 0;
+  while (i < 5) {
+    let service = prompt('Please enter a command.', 'D, W, B, or Q');
+    switch(service) {
+      case 'Q': {
+        break;
+      }
+      case 'D': {
+        prompt('Please enter the amount you want to deposit.');
+        break;
+      }
+      case 'W': {
+        prompt('Please enter the amount you want to withdraw.');
+        break;
+      }
+      case 'B': {
+        alert('Your balance is ' + total);
+        break;
+      }
+      default: {
+        prompt('Command not found.')
+      }
+    }
+    i++;
   }
 }
